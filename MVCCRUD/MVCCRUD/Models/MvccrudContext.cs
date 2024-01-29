90a9465dfa7ba6bdc6c83bc6c3035e4a4e0855ec
Alejandro-Jimenez-Daza <alejodaza061@gmail.com>
2024-01-28 19:32:43 -0500

    Se agrega el protecto CRUD

---
+++ MVCCRUD/MVCCRUD/Models/MvccrudContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCCRUD.Models;
+
+public partial class MvccrudContext : DbContext
+{
+    public MvccrudContext()
+    {
+    }
+
+    public MvccrudContext(DbContextOptions<MvccrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            //CADENA DE CONEXIÓN
+        }
+
+    }
+        
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3214EC07810242A6");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
